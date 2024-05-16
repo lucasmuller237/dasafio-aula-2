4cabc222e027974586ea0fbde8176a92983d6813
lucasmuller237 <160797156+lucasmuller237@users.noreply.github.com>
2024-05-16 20:36:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var  nome = prompt(`Olá bem vindo. Por favor diga o seu nome:`);
+
+var  idade = parseFloat(prompt(`${nome}, agora digite a sua idade`));
+
+var ano = 2024;
+
+var nascimento = ano-idade-1;
+
+
+ alert(`Você nasceu no ano de ${nascimento}`);
